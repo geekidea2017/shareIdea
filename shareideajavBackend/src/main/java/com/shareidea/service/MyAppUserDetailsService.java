0db02722c9ab2535ac451d67072ac144815a6fcd
Dhani <Dhani@192.168.0.6>
2017-08-30 15:54:51 +0530

    Architech design for base java program

---
+++ shareideajavBackend/src/main/java/com/shareidea/service/MyAppUserDetailsService.java
@@ -0,0 +1,30 @@
+package com.shareidea.service;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.shareidea.dao.IUserInfoDAO;
+import com.shareidea.entity.UserInfo;
+@Service
+public class MyAppUserDetailsService implements UserDetailsService {
+	@Autowired
+	private IUserInfoDAO userInfoDAO;
+	@Override
+	public UserDetails loadUserByUsername(String userName)
+			throws UsernameNotFoundException {
+		UserInfo activeUserInfo = userInfoDAO.getActiveUser(userName);
+		GrantedAuthority authority = new SimpleGrantedAuthority(activeUserInfo.getRole());
+		UserDetails userDetails = (UserDetails)new User(activeUserInfo.getUserName(),
+				activeUserInfo.getPassword(), Arrays.asList(authority));
+		return userDetails;
+	}
+}
+
